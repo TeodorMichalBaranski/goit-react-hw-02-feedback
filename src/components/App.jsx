f0202b4de4fd8e4b36b012822214eb5cb5321a38
TeodorMichalBaranski <126625089+TeodorMichalBaranski@users.noreply.github.com>
2024-02-20 22:17:32 +0100

    Feedback oprions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,28 @@
 import { Component } from 'react';
 import './App.css';
 import Section from './Section';
+import FeedbackOptions from './FeedbackOptions';
 
 export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  changeState = buttonName => {
+    this.state(prevState => ({
+      [buttonName]: prevState[buttonName] + 1,
+    }));
+  };
   render() {
     return (
       <div className="wrapper">
-        <Section title={'Please leave feedback'}></Section>
+        <Section title={'Please leave feedback'}>
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.changeState}
+          />
+        </Section>
       </div>
     );
   }
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -3,7 +3,7 @@ import css from './Section.module.css';
 const Section = ({ title, children }) => {
   return (
     <section className={css.section}>
-      <h1> {title}</h1>
+      <h3> {title}</h3>
       {children}
     </section>
   );
